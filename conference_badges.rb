a3fac41c4ac25589b4a80881ef5f3f60e22542d5
Amanda Walker <walker.akw@gmail.com>
2020-07-08 17:33:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+print badge_maker("Polly")
+
+def batch_badge_creator(attendees)
+  badge_array = []
+  attendees.each do |name|
+    badge_array << badge_maker(name)
+  end #each
+  badge_array
+end #batch_badge_creator
+print batch_badge_creator(["Ian", "Laura"])
